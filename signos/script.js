df5b621ef49b88ddfb81c34e4dc8b16c7791e1bb
PatrickLeonardo <educacaomidiatica7@gmail.com>
2022-08-11 18:29:33 -0300

    all_main

---
+++ signos/script.js
@@ -0,0 +1,18 @@
+function submit(){
+    if (document.getElementById('name').value.length == 0 || document.getElementById('month').value.length == 0){
+        alert('Nome ou Mês inválidos!')
+    }
+    else{
+        var month = document.getElementById('month').value
+
+        if (month.toLowerCase() == "março"){
+            alert("mes ok")
+        }
+        else{
+            alert('mes not ok')
+        }
+        var form = document.getElementById('form')
+        form.href = "http://127.0.0.1:5500/sdz/peixe/index.html"
+    }
+    console.log("ok")
+}
